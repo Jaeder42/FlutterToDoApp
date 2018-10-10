33974d0b396ce9181ef7ea2c7cdfd25accccb1d1
Johan Jäderblom <johan.jaderblom@valtech.se>
2018-10-10 16:29:25 +0200

    Changed som names

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,14 @@ import 'package:flutter/material.dart';
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
+      title: 'TODO App',
       theme: new ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage(title: 'TODO App'),
     );
   }
 }
